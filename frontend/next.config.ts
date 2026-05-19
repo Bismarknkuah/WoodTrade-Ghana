44035e8fd089323cbb7b3317d0fe042779daaa2a
Bismarknkuah <bismarknkuah@gmail.com>
2026-05-19 00:17:02 +0000

    fix next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -12,5 +11,4 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   },
 };
-
 export default nextConfig;
